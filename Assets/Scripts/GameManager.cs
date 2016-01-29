f124f4aa2f74a32e984b23089f594ab7fb4c8cc6
dMorenov <danielmorenotvjb42013@cev.com>
2016-01-29 21:25:21 +0100

    asdasd

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,53 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    public enum GameState { INTRO, GAMEPLAY, END}
+    public GameState state;
+
+    public ResourcesManager resources;
+
+    public float timer, maxDayTime;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        resources = GetComponent<ResourcesManager>();
+        timer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+        switch(state)
+        {
+            case GameState.INTRO:
+                break;
+            case GameState.GAMEPLAY:
+
+                timer += Time.deltaTime;
+
+                if (timer >= maxDayTime)
+                {
+                    //TODO: Change day
+
+                    timer = 0;
+                }
+
+                break;
+            case GameState.END:
+                break;
+        }
+
 	}
+
+    void LaunchRandomEvent()
+    {
+        //TODO: define daily random events
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
 }

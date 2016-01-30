dabac0f4f3ece30f068f919023c3df64c7f6744a
Marc <magueracoras@gmail.com>
2016-01-30 10:29:17 +0100

    QEIHBEHI

---
+++ Assets/Scripts/ResourcesEvents.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourcesEvents : MonoBehaviour {
+
+    public enum EventType { FOODEVENT, WATEREVENT, HEALTH }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
